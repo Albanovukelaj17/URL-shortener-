b105c1c150d218162d4112b927592a9236f460f7
Albano <albigoal@outlook.com>
2024-09-20 17:58:54 +0200

    retrieve

--- main.rb
+++ main.rb
@@ -11,12 +11,26 @@ class URLShortener
         @url_data[short_code]= long_url
         puts "Short URL: http://short.ly/#{short_code}"
     end
+    
+    def retrieve_url(short_url)
+        long_code =@url_data[short_url]
+        puts " Short URL is : http://short.ly/#{short_url}"
+
+        if long_code
+            puts " Original URL was : http://short.ly/#{long_code}"
+        else
+            puts " Input a valid short URL"
+        end
+    end
+ 
 end
+
+
 
 def test_url_shortener
     shortener = URLShortener.new  
     puts "Gib eine URL ein, um sie zu kürzen:"
-    long_url = gets.chomp  # Nimmt eine URL vom Benutzer entgegen
+    long_url = gets.chomp  # scan user
     shortener.shorten_url(long_url)  
   end
   
